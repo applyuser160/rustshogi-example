2eb3f7a726e8b69c7b85bfba839adcbc83e29896
applyuser160 <syunsuke130121@gamil.com>
2025-10-05 20:50:10 +0900

    update move test

--- tests/test_move.py
+++ tests/test_move.py
@@ -3,22 +3,51 @@ from rustshogi import Move, Address, ColorType, Piece, PieceType
 
 class TestMove:
     def test_move_standart(self):
-        mv = Move(csa="1a2b")
-        assert mv.is_drop() is False
-        assert mv.is_promote() is False
-        assert mv.get_from() == Address(1, 1)
-        assert mv.get_to() == Address(2, 2)
+        # Act
+        mv1 = Move(csa="1a2b")
+        mv2 = Move(from_address=Address(1, 1), to_address=Address(2, 2))
+
+        # Assert
+        assert mv1.is_drop() is False
+        assert mv1.is_promote() is False
+        assert mv1.get_from() == Address(1, 1)
+        assert mv1.get_to() == Address(2, 2)
+
+        assert mv2.is_drop() is False
+        assert mv2.is_promote() is False
+        assert mv2.get_from() == Address(1, 1)
+        assert mv2.get_to() == Address(2, 2)
 
     def test_move_drop(self):
-        mv = Move(csa="l*1a")
-        assert mv.is_drop() is True
-        assert mv.is_promote() is False
-        assert mv.get_piece() == Piece(ColorType.White, PieceType.Lance)
-        assert mv.get_to() == Address(1, 1)
+        # Act
+        mv1 = Move(csa="l*1a")
+        mv2 = Move(
+            piece=Piece(ColorType.White, PieceType.Lance), to_address=Address(1, 1)
+        )
+
+        # Assert
+        assert mv1.is_drop() is True
+        assert mv1.is_promote() is False
+        assert mv1.get_piece() == Piece(ColorType.White, PieceType.Lance)
+        assert mv1.get_to() == Address(1, 1)
+
+        assert mv2.is_drop() is True
+        assert mv2.is_promote() is False
+        assert mv2.get_piece() == Piece(ColorType.White, PieceType.Lance)
+        assert mv2.get_to() == Address(1, 1)
 
     def test_move_promote(self):
-        mv = Move(csa="1a2b+")
-        assert mv.is_drop() is False
-        assert mv.is_promote() is True
-        assert mv.get_from() == Address(1, 1)
-        assert mv.get_to() == Address(2, 2)
+        # Act
+        mv1 = Move(csa="1a2b+")
+        mv2 = Move(from_address=Address(1, 1), to_address=Address(2, 2), promote=True)
+
+        # Assert
+        assert mv1.is_drop() is False
+        assert mv1.is_promote() is True
+        assert mv1.get_from() == Address(1, 1)
+        assert mv1.get_to() == Address(2, 2)
+
+        assert mv2.is_drop() is False
+        assert mv2.is_promote() is True
+        assert mv2.get_from() == Address(1, 1)
+        assert mv2.get_to() == Address(2, 2)
